eba038aa001b2afc3c1d4a858e54217313574c66
Akshay Krishnan <53093783+akshaykrishnan003@users.noreply.github.com>
2021-01-20 23:47:35 +0530

    Add files via upload

---
+++ js/bird.js
@@ -0,0 +1,12 @@
+ 
+VANTA.BIRDS({
+  el: "#bird",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  colorMode: "lerp"
+})
