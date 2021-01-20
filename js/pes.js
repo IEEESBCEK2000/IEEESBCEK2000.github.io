6660622b8a6684b42b7e227e95b620feb43f22d0
Akshay Krishnan <53093783+akshaykrishnan003@users.noreply.github.com>
2021-01-21 00:41:58 +0530

    Update pes.js

--- js/pes.js
+++ js/pes.js
@@ -11,6 +11,17 @@ if (window.VANTA) {
   scaleMobile: 1.00,
   colorMode: "lerp"
 })
+}else{
+  window.VANTA.BIRDS({
+  el: ".pes",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  colorMode: "lerp"
 }
 }
 setVanta()
